5e8e72ccae12d75d96e0d5a879a9f0b7a47e7314
wolf052 <cmahur108@gmail.com>
2025-07-13 22:16:13 +0400

    tapsiriq4

---
+++ c# oyrenmek/Program.cs
@@ -0,0 +1,7 @@
+﻿int y = 60;
+Console.Write("Deqiqeni daxil et: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int z = (x % y);
+int e = (x / y);
+Console.WriteLine("Cemi deqiqe> " + x);
+Console.WriteLine("Netice> " + e +" saat " + z + " deqiqe" );
